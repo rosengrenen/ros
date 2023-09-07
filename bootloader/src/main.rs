309845fd455148abda98c4a0ead8f093b780b91f
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2023-09-07 14:03:55 +0200

    return slices instead of bytes read, maybe we should do both, but automate the num

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -705,179 +705,176 @@ fn print_dsdt(dsdt_addr: u64) {
     parse_definition_block(&aml_slice);
 }
 
-fn parse_definition_block(aml: &[u8]) {
-    let mut offset = 0;
+fn parse_definition_block(mut input: &[u8]) {
+    // let mut offset = 0;
     loop {
-        let ty = aml[offset];
-        offset += 1;
-        sprintln!("Block type {:x} at offset {:x}", ty, offset);
+        let (new_input, ty) = parse_block_type(input);
+        input = new_input;
+        // sprintln!("Block type {:x} at offset {:x}", ty, offset);
         match ty {
             0x10 => {
-                let (_, len) = parse_def_scope(&aml[offset..]);
-                offset += len;
+                let (new_input, _) = parse_def_scope(input);
+                input = new_input;
             }
             _ => {
-                panic!("unknown block type {:x} at {}", ty, offset - 1);
+                panic!("unknown block type {:x}", ty);
             }
         }
     }
 }
 
-fn parse_def_scope(aml: &[u8]) -> ParseResult<()> {
-    let (pkg_length, read_bytes) = parse_pkg_length(aml);
-    let aml = &aml[..pkg_length];
-    parse_name_string(&aml[read_bytes..]);
-    ((), pkg_length)
+fn parse_block_type(input: &[u8]) -> ParseResult<u8> {
+    (&input[1..], input[0])
 }
 
-fn parse_pkg_length(aml: &[u8]) -> ParseResult<usize> {
-    let extra_bytes_count = (aml[0] >> 6) as usize;
+fn parse_def_scope(input: &[u8]) -> ParseResult<()> {
+    let (input, pkg_length) = parse_pkg_length(input);
+    parse_name_string(input);
+    (&input[pkg_length..], ())
+}
+
+fn parse_pkg_length(input: &[u8]) -> ParseResult<usize> {
+    let extra_bytes_count = (input[0] >> 6) as usize;
     let pkg_length = if extra_bytes_count == 0 {
-        aml[0] as usize
+        input[0] as usize
     } else {
-        assert_eq!(aml[0] & 0b0011_0000, 0);
-        let mut pkg_length = (aml[0] & 0xf) as usize;
+        assert_eq!(input[0] & 0b0011_0000, 0);
+        let mut pkg_length = (input[0] & 0xf) as usize;
         for i in 1..=extra_bytes_count {
-            pkg_length |= (aml[i] as usize) << (i * 8 - 4);
+            pkg_length |= (input[i] as usize) << (i * 8 - 4);
         }
 
         pkg_length
     };
-    sprintln!("Pkg len: {}", pkg_length);
-    (pkg_length, extra_bytes_count + 1)
+    (
+        &input[extra_bytes_count + 1..],
+        pkg_length - (extra_bytes_count + 1),
+    )
 }
 
-fn parse_name_string(aml: &[u8]) -> ParseResult<()> {
-    if let Some((_, bytes_parsed)) = parse_root_char(aml) {
-        parse_name_path(&aml[bytes_parsed..]);
+fn parse_name_string(input: &[u8]) -> ParseResult<()> {
+    if let Some((input, _)) = parse_root_char(input) {
+        return parse_name_path(input);
     } else {
-        let (_, bytes_parsed) = parse_prefix_path(aml);
-        parse_name_path(&aml[bytes_parsed..]);
+        let (input, _) = parse_prefix_path(input);
+        return parse_name_path(input);
     }
-    //
-    // root \
-    // prefix ^
-    ((), 0)
 }
 
-fn parse_root_char(aml: &[u8]) -> Option<ParseResult<()>> {
-    if aml[0] == b'\\' {
-        return Some(((), 1));
+fn parse_root_char(input: &[u8]) -> Option<ParseResult<()>> {
+    if input[0] == b'\\' {
+        return Some((&input[1..], ()));
     }
 
     None
 }
 
-fn parse_prefix_path(aml: &[u8]) -> ParseResult<usize> {
+fn parse_prefix_path(mut input: &[u8]) -> ParseResult<usize> {
     let mut i = 0;
     loop {
-        if aml[i] == b'^' {
+        if input[i] == b'^' {
             i += 1;
+            input = &input[1..];
             continue;
         }
 
         break;
     }
 
-    (i, i)
+    (input, i)
 }
 
-fn parse_name_path(aml: &[u8]) -> ParseResult<()> {
-    if let Some((name_seg, bytes_read)) = parse_name_seg(aml) {
+fn parse_name_path(input: &[u8]) -> ParseResult<()> {
+    if let Some((input, name_seg)) = parse_name_seg(input) {
         sprintln!("{}", core::str::from_utf8(&name_seg).unwrap());
-        return ((), bytes_read);
+        return (input, ());
     }
 
-    if let Some((name_seg, bytes_read)) = parse_dual_name_path(aml) {
+    if let Some((input, name_seg)) = parse_dual_name_path(input) {
         sprintln!("{}", core::str::from_utf8(&name_seg).unwrap());
-        return ((), bytes_read);
+        return (input, ());
     }
 
-    if let Some((_, bytes_read)) = parse_null_name(aml) {
-        return ((), bytes_read);
+    if let Some((input, _)) = parse_null_name(input) {
+        return (input, ());
     }
 
     unimplemented!();
 }
 
-fn parse_dual_name_path(aml: &[u8]) -> Option<ParseResult<[u8; 8]>> {
+fn parse_dual_name_path(mut input: &[u8]) -> Option<ParseResult<[u8; 8]>> {
     let mut total_bytes_read = 1;
-    if aml[0] != b'.' {
+    if input[0] != b'.' {
         return None;
     }
 
+    input = &input[1..];
+
     let mut name_path = [0u8; 8];
-    if let Some((name_seg, bytes_read)) = parse_name_seg(&aml[total_bytes_read..]) {
+    if let Some((input, name_seg)) = parse_name_seg(input) {
         name_path[0] = name_seg[0];
         name_path[1] = name_seg[1];
         name_path[2] = name_seg[2];
         name_path[3] = name_seg[3];
-        total_bytes_read += bytes_read;
     }
 
-    if let Some((name_seg, bytes_read)) = parse_name_seg(&aml[total_bytes_read..]) {
+    if let Some((input, name_seg)) = parse_name_seg(input) {
         name_path[4] = name_seg[0];
         name_path[5] = name_seg[1];
         name_path[6] = name_seg[2];
         name_path[7] = name_seg[3];
-        total_bytes_read += bytes_read;
     }
 
-    Some((name_path, total_bytes_read))
+    Some((input, name_path))
 }
 
-fn parse_null_name(aml: &[u8]) -> Option<ParseResult<()>> {
-    if aml[0] == 0 {
-        return Some(((), 1));
+fn parse_null_name(input: &[u8]) -> Option<ParseResult<()>> {
+    if input[0] == 0 {
+        return Some((&input[1..], ()));
     }
 
     None
 }
 
-fn parse_name_seg(aml: &[u8]) -> Option<ParseResult<[u8; 4]>> {
+fn parse_name_seg(input: &[u8]) -> Option<ParseResult<[u8; 4]>> {
     let mut name_seg = [0; 4];
-    let mut total_bytes_read = 0;
 
-    let (char, bytes_read) = parse_lead_name_char(&aml[total_bytes_read..])?;
-    total_bytes_read += bytes_read;
+    let (input, char) = parse_lead_name_char(input)?;
     name_seg[0] = char;
-    let (char, bytes_read) = parse_name_char(&aml[total_bytes_read..])?;
-    total_bytes_read += bytes_read;
+    let (input, char) = parse_name_char(input)?;
     name_seg[1] = char;
-    let (char, bytes_read) = parse_name_char(&aml[total_bytes_read..])?;
-    total_bytes_read += bytes_read;
+    let (input, char) = parse_name_char(input)?;
     name_seg[2] = char;
-    let (char, bytes_read) = parse_name_char(&aml[total_bytes_read..])?;
-    total_bytes_read += bytes_read;
+    let (input, char) = parse_name_char(input)?;
     name_seg[3] = char;
 
-    Some((name_seg, total_bytes_read))
+    Some((input, name_seg))
 }
 
-fn parse_lead_name_char(aml: &[u8]) -> Option<ParseResult<u8>> {
-    let b = aml[0];
+fn parse_lead_name_char(input: &[u8]) -> Option<ParseResult<u8>> {
+    let b = input[0];
     if b == b'_' || (b >= b'A' && b <= b'Z') {
-        return Some((b, 1));
+        return Some((&input[1..], b));
     }
 
     None
 }
 
-fn parse_digit_char(aml: &[u8]) -> Option<ParseResult<u8>> {
-    let b = aml[0];
+fn parse_digit_char(input: &[u8]) -> Option<ParseResult<u8>> {
+    let b = input[0];
     if b >= b'0' && b <= b'9' {
-        return Some((b, 1));
+        return Some((&input[1..], b));
     }
 
     None
 }
 
-fn parse_name_char(aml: &[u8]) -> Option<ParseResult<u8>> {
-    if let Some(r) = parse_digit_char(aml) {
+fn parse_name_char(input: &[u8]) -> Option<ParseResult<u8>> {
+    if let Some(r) = parse_digit_char(input) {
         return Some(r);
     };
 
-    parse_lead_name_char(aml)
+    parse_lead_name_char(input)
 }
 
-type ParseResult<T> = (T, usize);
+type ParseResult<'a, T> = (&'a [u8], T);
