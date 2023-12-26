b59ef1c2e5050254bb7cbab3a43109d6f84ad202
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2023-12-26 13:15:09 +0100

    restructure kernel dir

--- bootloader/build-and-run.sh
+++ bootloader/build-and-run.sh
@@ -11,8 +11,8 @@ mformat -i fat.img ::
 mmd -i fat.img ::/EFI
 mmd -i fat.img ::/EFI/BOOT
 mcopy -i fat.img target/x86_64-unknown-uefi/debug/BOOTX64.EFI ::/EFI/BOOT
-cd ../kernel && cargo build --release && cd -
-mcopy -i fat.img ../kernel/target/x86_64/release/ros ::/
+cd ../kernel/ros && cargo build --release && cd -
+mcopy -i fat.img ../kernel/ros/target/x86_64/release/ros ::/
 
 mkgpt -o hdimage.bin --part fat.img --type system
 # sudo qemu-system-x86_64 -m 1G -L /usr/share/ovmf/x64 -pflash /usr/share/ovmf/x64/OVMF.fd -hda hdimage.bin -serial stdio -no-reboot
